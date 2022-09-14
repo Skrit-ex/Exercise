c7a68906c71710bf2348753b18c18d439bb023c8
Skrit-ex <skritex0@gmail.com>
2022-09-14 19:23:38 +0300

    add

---
+++ src/LocalTime/LocalTime2.java
@@ -0,0 +1,19 @@
+package LocalTime;
+
+import java.time.LocalTime;
+
+public class LocalTime2 {
+    public static void main(String[] args) {
+        LocalTime time = LocalTime.now();
+    int hour = time.getHour();
+    int minute = time.getMinute();
+    int second = time.getSecond();
+
+       // System.out.println("Часы " + hour);
+       // System.out.println("Минуты " + minute);
+       // System.out.println("Секунды " + second);
+
+
+        System.out.println(" Текущее время : " + time + minute + second);
+    }
+}
