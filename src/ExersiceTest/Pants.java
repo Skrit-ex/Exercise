db24d6bf78ba8ebbc03fc95f6cf09babe2a2d0ab
Skrit-ex <skritex0@gmail.com>
2022-09-20 22:02:47 +0300

    add

---
+++ src/ExersiceTest/Pants.java
@@ -0,0 +1,17 @@
+package ExersiceTest;
+
+public class Pants extends Tovar implements FemaleClothes, MaleClothes{
+    public Pants(Sizes size, String price, String color) {
+        super(size, price, color);
+
+    }
+
+    @Override
+    public String toString() {
+        return "Штаны{" +
+                "размер = " + getSize() +
+                ", цена = " + getPrice() + " $" +
+                ", цвет = " + getColor() +
+                "}";
+    }
+}
---
+++ src/ExersiceTest/Tie.java
@@ -0,0 +1,15 @@
+package ExersiceTest;
+
+public class Tie extends Tovar implements FemaleClothes, MaleClothes {
+    public Tie( String price, String color) {
+        super(price, color);
+    }
+
+    @Override
+    public String toString() {
+        return "Галстук{" +
+                " цена = " + getPrice() + " $" +
+                ", цвет = " + getColor() +
+                "}";
+    }
+}
