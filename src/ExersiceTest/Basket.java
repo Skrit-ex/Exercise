3aaa2d47618788c95a2da626f1d54bfe785cfe87
Skrit-ex <skritex0@gmail.com>
2022-09-27 22:14:54 +0300

    add

--- src/ExersiceTest/Basket.java
+++ src/ExersiceTest/Basket.java
@@ -1,22 +1,20 @@
 package ExersiceTest;
 
 public class Basket {
+    public  void backTovar(String katal){
+        System.out.println("Товары в корзине");
 
-    Tovar [] buyTovars = new Tovar[3];
-
-    public Basket(Tovar[] buyTovars) {
-        this.buyTovars = buyTovars;
+        String mes = switch (katal) {
+            case "1" -> "Товар Штаны (чёрного цвета) добавлен в корзину";
+            case "2" -> "Товар Штаны (желтого цвета) добавлен в корзину";
+            case "3" -> "Товар Майка (белого цвета) добавлен в корзину";
+            case "4" -> "Товар Майка (чёрного цвета) добавлен в корзину";
+            case "5" -> "Товар Галстук (розового цвета) добавлен в корзину";
+            case "6" -> "Товар Галстук (чёрного цвета) добавлен в корзину";
+            case "7" -> "Товар Часы (белого цвета) добавлен в корзину";
+            case "8" -> "Товар Часы (серого цвета) добавлен в корзину";
+            default -> "Неизвестный товар : " + katal;
+        };
+        System.out.println(mes);
     }
-
-    public Basket() {
-    }
-
-    public Tovar[] getBuyTovars() {
-        return buyTovars;
-    }
-
-    public void setBuyTovars(Tovar[] buyTovars) {
-        this.buyTovars = buyTovars;
-    }
-
 }
