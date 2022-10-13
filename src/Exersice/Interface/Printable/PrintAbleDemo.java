d8aa6c635322cc5b00f69ad137661dfbf646ec64
Skrit-ex <skritex0@gmail.com>
2022-10-13 21:07:13 +0300

    add

--- src/Exersice/Interface/Printable/PrintAbleDemo.java
+++ src/Exersice/Interface/Printable/PrintAbleDemo.java
@@ -7,13 +7,21 @@ public class PrintAbleDemo {
         Magazine magazine = new Magazine("Kept");
         Magazine magazine1 = new Magazine("List");
 
-        Printable [] printables = {book,book1,magazine,magazine1};
-      //  for( Printable printable: printables){
-            //printable.print();
 
-            Book.printBooks(printables);
-        System.out.println(" ");
-            Magazine.printMagazine(printables);
+        Printable printable1 = new Printable() {
+            @Override
+            public void print() {
+                System.out.println("Printing demo "); //  задание "Вложенные классы"
+            }
+        };
+        Printable[] printables = {book, book1, magazine, magazine1, printable1};
+        for (Printable printable : printables) {
+            printable.print();
+
+
+//        Book.printBooks(printables);
+//        System.out.println(" ");
+//            Magazine.printMagazine(printables);
         }
     }
-
+}
