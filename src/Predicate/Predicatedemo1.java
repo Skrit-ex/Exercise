e0d00feed2d90c5aac2a55885bc34d92102cdbec
Skrit-ex <skritex0@gmail.com>
2022-10-17 18:39:55 +0300

    add

--- src/Predicate/Predicatedemo1.java
+++ src/Predicate/Predicatedemo1.java
@@ -1,4 +1,22 @@
 package Predicate;
 
+import java.util.function.Predicate;
+
 public class Predicatedemo1 {
+    public static void main(String[] args) {
+
+//        Predicate<String> predicate = new Predicate<String>() {
+//            @Override
+//            public boolean test(String s) {
+//                return s!=null;
+//            }
+//        };
+
+        // используя лямбда выражения
+         Predicate<String> predicate = s -> s!= null;
+
+        System.out.println(predicate.test("12"));
+        System.out.println(predicate.test(null));
+        System.out.println(predicate.test(""));
+    }
 }
