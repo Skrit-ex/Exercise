14e7c71e5a37b6b31b7b464f9fd4d5db608bcc64
Skrit-ex <skritex0@gmail.com>
2022-10-17 21:02:20 +0300

    add

---
+++ src/HomeWork/Predicate/PredicateDemo.java
@@ -0,0 +1,16 @@
+package HomeWork.Predicate;
+
+import java.util.function.Predicate;
+
+public class PredicateDemo {
+    public static void main(String[] args) {
+        Predicate <String> predicate = s -> s.startsWith("J");
+        Predicate <String> predicate1 = s -> s.startsWith("N");
+        Predicate <String> predicate2 = s -> s.endsWith("A");
+
+        Predicate<String> predicateTest = predicate.or(predicate1).and(predicate2);
+        System.out.println(predicateTest.test("Java"));
+        System.out.println(predicateTest.test("JavA"));
+        System.out.println(predicateTest.test("Next"));
+    }
+}
