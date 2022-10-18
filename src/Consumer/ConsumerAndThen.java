55f1f7dd99fba2729c7fb1e63fad71d274129f52
Skrit-ex <skritex0@gmail.com>
2022-10-18 20:49:29 +0300

    add

---
+++ src/Consumer/ConsumerAndThen.java
@@ -0,0 +1,12 @@
+package Consumer;
+
+import java.util.function.Consumer;
+
+public class ConsumerAndThen {
+    public static void main(String[] args) {
+        Consumer<String> stringConsumer1 = string -> System.out.println(string.toUpperCase());
+        Consumer<String> stringConsumer2 = string -> System.out.println(string.toLowerCase());
+
+        stringConsumer1.andThen(stringConsumer2).accept("you must be free");
+    }
+}
