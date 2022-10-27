9ba2125c0db1b30a87e9509e2db5933e665e8270
Skrit-ex <skritex0@gmail.com>
2022-10-27 21:10:46 +0300

    add

---
+++ src/Collection/ArrayList/RetainAllDemo.java
@@ -0,0 +1,28 @@
+package Collection.ArrayList;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class RetainAllDemo {
+    public static void main(String[] args) {
+        List<String> array = new ArrayList<>();
+        List<String> array1 = Arrays.asList("AA", "ASA", "BSA");
+
+
+        array.add("AA");
+        array.add("B");
+        array.add("C");
+        array.add("ASA");
+        array.add("FE");
+        array.add("E");
+
+        System.out.println("Размер array до удаления: "
+                + array.size());
+
+        array.retainAll(array1);
+        System.out.println(array);
+        System.out.println("Размер array после удаления: "
+                + array.size());
+    }
+}
