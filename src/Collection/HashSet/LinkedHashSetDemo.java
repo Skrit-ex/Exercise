1e8b6bcab39fa88544fa7062b7c3e287142bb324
Skrit-ex <skritex0@gmail.com>
2022-11-01 21:07:06 +0300

    add

---
+++ src/Collection/HashSet/LinkedHashSetDemo.java
@@ -0,0 +1,19 @@
+package Collection.HashSet;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class LinkedHashSetDemo {
+    public static void main(String[] args) {
+        Set<String> set = new LinkedHashSet<>();
+
+        set.add("Бета");
+        set.add("Aльфa");
+        set.add("Этa");
+        set.add("Гaммa");
+        set.add("Эпсилон");
+        set.add("Oмeгa");
+
+        System.out.println(set);
+    }
+}
