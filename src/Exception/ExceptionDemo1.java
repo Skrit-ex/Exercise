93331567ee3e18becd35be144b0a743003152987
Skrit-ex <skritex0@gmail.com>
2022-12-03 19:25:50 +0300

    add

---
+++ src/Exception/ExceptionDemo1.java
@@ -0,0 +1,19 @@
+package Exception;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class ExceptionDemo1 {
+    public static void main(String[] args) {
+        File file = new File("dawd");
+
+        try {
+            Scanner scanner = new Scanner(file);
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+        }
+
+
+    }
+}
