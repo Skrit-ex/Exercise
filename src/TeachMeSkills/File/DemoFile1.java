df58b30be339c48772ddf13803acdf5d328be75b
Skrit-ex <skritex0@gmail.com>
2023-05-24 21:47:27 +0300

    add

--- src/TeachMeSkills/File/DemoFile1.java
+++ src/TeachMeSkills/File/DemoFile1.java
@@ -1,4 +1,20 @@
 package TeachMeSkills.File;
 
+import java.io.*;
+import java.util.Arrays;
+
+
 public class DemoFile1 {
+    public static void main(String[] args) throws IOException {
+
+        File file = new File("G:\\JDK\\src\\TeachMeSkills\\File\\text.txt");
+
+        InputStream fis = new FileInputStream(file);
+
+        byte [] i = new byte[500];
+        int read = fis.read(i);
+        System.out.println(Arrays.toString(i));
+        System.out.print((char) read);
+        fis.close();
+    }
 }
