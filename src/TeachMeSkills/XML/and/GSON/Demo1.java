f683599e7d7c03cea2f2ae69f05125ee6650be35
Skrit-ex <skritex0@gmail.com>
2023-06-27 00:13:59 +0300

    add

--- src/TeachMeSkills/XML/and/GSON/Demo1.java
+++ src/TeachMeSkills/XML/and/GSON/Demo1.java
@@ -2,8 +2,17 @@ package TeachMeSkills.XML.and.GSON;
 
 import com.google.gson.Gson;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Demo1 {
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException {
+
+        //1 пример
         Gson gson = new Gson();
         User user1 = new User("Kira","Kira", "kira");
         User user2 = new User("Dima","Dima", "Dima");
@@ -14,6 +23,38 @@ public class Demo1 {
         System.out.println(s1);
         System.out.println(s2);
 
+        // через лист, если передавать массив
+        List<User> users = new ArrayList<>();
+        users.add(user1);
+        users.add(user2);
+
+        String s = gson.toJson(users);
+        System.out.println(s);
+//
+//        // если имеются данные gson (можно быстро преобразовать, записав в файл, смотри ниже)
+//        List list = gson.fromJson("[{\"name\": \"Alex\",\"username\": \"simon\",\"password\": \"simon\",\"age\": \"23\",\"telephone\": \"164616161\"}," +
+//                "{\"name\": \"Alex1\",\"username\": \"simon1\",\"password\": \"simon2\",\"age\": \"24\",\"telephone\": \"16214412161\"}]", List.class);
+//
+//        System.out.println(list);
+
+
+//        //Запись в файл
+//
+//        FileWriter fileWriter = new FileWriter("G:\\JDK\\Exercises\\src\\TeachMeSkills\\XML\\textDemo.json");
+//        fileWriter.write(s); //s из примера 1
+//        fileWriter.close();
+
+
+        // чтение из gson
+        FileReader fileReader = new FileReader("G:\\JDK\\Exercises\\src\\TeachMeSkills\\XML\\textDemo.json");
+        BufferedReader bufferedReader = new BufferedReader(fileReader);
+        String s3 = bufferedReader.readLine();
+        System.out.println(s3);
+        List list1 = gson.fromJson(s3, List.class);
+        System.out.println(list1);
+
+
+//
 //        DocumentBuilderFactory doc = DocumentBuilderFactory.newInstance(); //cоздаём фабрику
 //        DocumentBuilder documentBuilder = doc.newDocumentBuilder();   // просим у фабрики создать документ
 //        Document parse = documentBuilder.parse(new File("G:\\JDK\\Exercises\\src\\TeachMeSkills\\XML\\text.xml")); //парсим документ
