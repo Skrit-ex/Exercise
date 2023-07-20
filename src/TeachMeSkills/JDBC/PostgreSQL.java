c171b2ead3ed77b8c695d927d04be3f9ccded615
Skrit-ex <skritex0@gmail.com>
2023-07-20 11:03:22 +0300

    add

--- src/TeachMeSkills/JDBC/PostgreSQL.java
+++ src/TeachMeSkills/JDBC/PostgreSQL.java
@@ -1,7 +1,29 @@
 package TeachMeSkills.JDBC;
 
-public class PostgreSQL {
-    public static void main(String[] args) {
+import java.sql.*;
 
+public class PostgreSQL {
+    public static void main(String[] args) throws SQLException {
+        // 1 connection
+    Connection connection =
+        DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres", "postgres", "admin");
+
+    //  2 statement
+//
+//        Statement statement = connection.createStatement();
+//        statement.execute("insert into operation ( num1, num2, type, result) values ( 25, 25, 'sum', 50)");
+
+        // 3 prepareStatement
+        PreparedStatement preparedStatement =
+           connection.prepareStatement("insert into operation ( num1, num2, type, result) values ( ?, ?, ?, ?)");
+            preparedStatement.setInt(1,23);
+            preparedStatement.setInt(2,23);
+            preparedStatement.setString(3,"sum");
+            preparedStatement.setInt(4,46);
+            preparedStatement.execute();
+
+        // 4 resultSet
+        Statement statement = connection.createStatement();
+        statement.executeQuery("select  * from operation where type='sum'");
     }
 }
