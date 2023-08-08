8153b032f227a471efa1cf3183c2f16b6c43ca42
Skrit-ex <skritex0@gmail.com>
2023-08-08 12:29:40 +0300

    add

--- src/TeachMeSkills/Web/CalculatorWeb.java
+++ src/TeachMeSkills/Web/CalculatorWeb.java
@@ -3,12 +3,28 @@ package TeachMeSkills.Web;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+
 public class CalculatorWeb implements HttpHandler {
     @Override
     public void handle(HttpExchange exchange) throws IOException {
-        System.out.println(exchange.getRequestURI().getQuery());
+        //System.out.println(exchange.getRequestURI().getQuery());
         //сплит в масив
         String[] split = exchange.getRequestURI().getQuery().split("&");
-        System.out.println(split);
+        String num1 = split[0].split("=")[1];
+        String num2 = split[1].split("=")[1];
+        String type = split[2].split("=")[1];
+
+        System.out.println(type);
+
+
+        String s = "Result = 4";
+        exchange.sendResponseHeaders(200, s.length());
+        OutputStream responseBody =  exchange.getResponseBody();
+        PrintWriter printWriter = new PrintWriter(responseBody);
+        printWriter.print(s);
+        printWriter.close();
+
     }
 }
