fb24489c066ada684f039c43ba6cff934cbe2ef8
Skrit-ex <skritex0@gmail.com>
2024-01-09 00:34:02 +0300

    add

--- src/TeachMeSkills/JDBCadvanced/Test.java
+++ src/TeachMeSkills/JDBCadvanced/Test.java
@@ -29,24 +29,42 @@ public class Test {
 //        preparedStatement.execute();
 
 
-        //2 вариант занесения в таблицу
-        PreparedStatement preparedStatement = connection.prepareStatement("insert into account values (default, ?,?,?)");
-        preparedStatement.setString(1, "test2");
-        preparedStatement.setString(2, "test2");
-        preparedStatement.setString(3, "test2");
-        preparedStatement.execute();
+//        //2 вариант занесения в таблицу
+//        PreparedStatement preparedStatement = connection.prepareStatement("insert into account values (default, ?,?,?)");
+//        preparedStatement.setString(1, "test2");
+//        preparedStatement.setString(2, "test2");
+//        preparedStatement.setString(3, "test2");
+//        preparedStatement.execute();
 
 
         //работа с результатом
 
-        ResultSet resultSet = preparedStatement.executeQuery();
-        while (resultSet.next()) {
-            int id = resultSet.getInt(1);
-            String name = resultSet.getString(2);
-            String username = resultSet.getString(3);
-            String password = resultSet.getString(4);
+//       PreparedStatement preparedStatement1 = connection.prepareStatement("select * from account");
+//        ResultSet resultSet = preparedStatement1.executeQuery();
+//        while (resultSet.next()) {
+//            int id = resultSet.getInt(1);
+//            String name = resultSet.getString(2);
+//            String username = resultSet.getString(3);
+//            String password = resultSet.getString(4);
+//
+//            System.out.printf("%s,%s,%s,%s \n", id, name, username, password);
 
-            System.out.printf("%s,%s,%s,%s", id, name, username, password);
-        }
+        // работа по связыванию баз данных через ключи
+
+        PreparedStatement preparedStatement = connection.prepareStatement("insert into address values (default, ?) returning id"); // для получение id
+        preparedStatement.setString(1, "Test");
+        ResultSet resultSet = preparedStatement.executeQuery();
+        resultSet.next();
+        int addressId = resultSet.getInt(1);
+
+        PreparedStatement preparedStatement1 = connection.prepareStatement("insert into account values (default, ?, ?, ?,?)");
+        preparedStatement1.setString(1, "test");
+        preparedStatement1.setString(2, "test");
+        preparedStatement1.setString(3, "test");
+        preparedStatement1.setInt(4, addressId);
+        preparedStatement1.execute();
+
+
     }
 }
+
