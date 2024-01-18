130e27b8373187097e524a8f382ce4962cbc250f
Skrit-ex <skritex0@gmail.com>
2024-01-18 14:55:51 +0300

    add

--- src/TeachMeSkills/CollectionAdvanced/Test.java
+++ src/TeachMeSkills/CollectionAdvanced/Test.java
@@ -1,8 +1,6 @@
 package TeachMeSkills.CollectionAdvanced;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 public class Test {
     public static void main(String[] args) {
@@ -15,17 +13,38 @@ public class Test {
 //
 //        List<String>a = new LinkedList<>();
 
+//
+//        List<Integer> a1 = new ArrayList<>();
+//        for(int i=0; i< 100_000_000; i++){
+//            a1.add(i);
+//        }
+//
+//        long start = System.nanoTime();
+//        Integer integer = a1.get(67_900_900);
+//        long end = System.nanoTime();
+//        System.out.println(end-start);
+//
+//
 
-        List<Integer> a1 = new ArrayList<>();
-        for(int i=0; i< 100_000_000; i++){
-            a1.add(i);
-        }
 
-        long start = System.nanoTime();
-        Integer integer = a1.get(67_900_900);
-        long end = System.nanoTime();
-        System.out.println(end-start);
+        // set
+        //HashSet
+        //TreeSet
 
+        Set<String> set = new HashSet<>();   //данные не дублируются
+        set.add("ACB");
+        set.add("ACT");
+        set.add("CAR");
+        set.add("ACB");
 
+        System.out.println(set);
+
+        Set<String> set1 = new TreeSet<>();  // если  требуется сортировка
+        set1.add("BBB");
+        set1.add("DDD");
+        set1.add("AAC");
+        set1.add("AAA");
+
+        System.out.println(set1);
     }
 }
