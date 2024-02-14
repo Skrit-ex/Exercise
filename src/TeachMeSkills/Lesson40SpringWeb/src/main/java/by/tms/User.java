345418ccf084bbfdcca1064c5e153b80e41c79ee
Skrit-ex <skritex0@gmail.com>
2024-02-14 18:56:43 +0300

    add

--- src/TeachMeSkills/Lesson40SpringWeb/src/main/java/by/tms/User.java
+++ src/TeachMeSkills/Lesson40SpringWeb/src/main/java/by/tms/User.java
@@ -4,12 +4,21 @@ package by.tms;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.NotBlank;
+import org.hibernate.validator.constraints.NotEmpty;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class User {
+
+    @NotBlank
+    @NotEmpty
     private String name;
+    @NotBlank
+    @NotEmpty
     private String username;
+    @NotBlank
+    @NotEmpty
     private String password;
 }
