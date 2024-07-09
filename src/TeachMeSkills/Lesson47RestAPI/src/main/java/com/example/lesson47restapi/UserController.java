42988bc9b2f084d0f3ec545fd0b4f587f948c990
Skrit-ex <skritex0@gmail.com>
2024-07-09 15:35:09 +0300

    added upload and download image

--- src/TeachMeSkills/Lesson47RestAPI/src/main/java/com/example/lesson47restapi/UserController.java
+++ src/TeachMeSkills/Lesson47RestAPI/src/main/java/com/example/lesson47restapi/UserController.java
@@ -1,13 +1,19 @@
 package com.example.lesson47restapi;
 
 
+import lombok.SneakyThrows;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.server.reactive.HttpHandler;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/user")
@@ -16,25 +22,33 @@ public class UserController {
 
     private final List<User> users = new ArrayList<>();
 
+    private final Map<String, byte[]> avatar = new HashMap<>();
 
-    @PostMapping("/save")
-    public ResponseEntity<User> save(@RequestBody User user){
+    @PostMapping
+    public ResponseEntity<User> saveUser(@RequestBody User user) {
         users.add(user);
-        return  ResponseEntity.ok(user);
+        return ResponseEntity.ok(user);
+    }
+
+
+    @PostMapping("/{username}/save")
+    public ResponseEntity<User> save(@RequestBody User user) {
+        users.add(user);
+        return ResponseEntity.ok(user);
     }
 
     @GetMapping
-    private ResponseEntity<List<User>> findAll(){
+    private ResponseEntity<List<User>> findAll() {
         return ResponseEntity.ok(users);
     }
 
     @GetMapping("{username}")
-    public ResponseEntity<User> findByUsername(@PathVariable String username){
-        if(username.equals("ADMIN")){
+    public ResponseEntity<User> findByUsername(@PathVariable String username) {
+        if (username.equals("ADMIN")) {
             throw new RuntimeException("Error");
         }
-        for(User user:users){
-            if(user.getUsername().equals(username)){
+        for (User user : users) {
+            if (user.getUsername().equals(username)) {
                 return ResponseEntity.ok(user);
             }
         }
@@ -42,17 +56,29 @@ public class UserController {
     }
 
     @DeleteMapping("/{username}")
-    public ResponseEntity<Void> deleteByUsername(@PathVariable String username){
-        if(users.removeIf(user -> user.getUsername().equals(username))){
+    public ResponseEntity<Void> deleteByUsername(@PathVariable String username) {
+        if (users.removeIf(user -> user.getUsername().equals(username))) {
             return ResponseEntity.ok().build();
         }
         return ResponseEntity.notFound().build();
     }
 
 
-    @PostMapping("/upload")
-    public void uploadAvatar (@PathVariable String username,
-                                  MultipartFile file){
-        System.out.println(file);
+    @SneakyThrows  //annotation lombok exception
+    @PostMapping("/{username}/upload")
+    public ResponseEntity<Void> uploadAvatar(@PathVariable String username,
+                                             MultipartFile file) {
+        if (file.isEmpty()) {
+            return ResponseEntity.badRequest().build();
+        }
+        avatar.put(username, file.getBytes());
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/{username}/download")
+    public ResponseEntity<byte[]> downloadAvatar(@PathVariable String username){
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add(HttpHeaders.CONTENT_TYPE, MediaType.IMAGE_JPEG_VALUE);
+        return new ResponseEntity<>(avatar.get(username), httpHeaders ,HttpStatus.OK);
     }
 }
