de37c813926f9a3f97a88e49e90ecbe77919cd46
Skrit-ex <skritex0@gmail.com>
2024-08-01 19:32:33 +0300

    added

--- src/TeachMeSkills/Lesson49SpringDataJPA/src/main/java/com/example/lesson49springdatajpa/UserController.java
+++ src/TeachMeSkills/Lesson49SpringDataJPA/src/main/java/com/example/lesson49springdatajpa/UserController.java
@@ -2,7 +2,9 @@ package com.example.lesson49springdatajpa;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -14,14 +16,24 @@ public class UserController {
     private UserRepository userRepository;
 
     @GetMapping("/reg")
-    public String reg(){
+    public String reg(Model model){
+        model.addAttribute("newUser", new User());
         return "index";
     }
 
     @PostMapping("/reg")
-    public String reg(User user){
+    public String reg(@ModelAttribute("newUser") User user){
         userRepository.save(user);
         return "redirect:/";
 
+    }
+
+    @GetMapping("/auth")
+    public String auth(){
+        return "index";
+    }
+    @PostMapping("/auth")
+    public String auth(User user){
+        return "index";
     }
 }
