e6e470eb36537b7655800c02cd6af87495a79495
Skrit-ex <skritex0@gmail.com>
2024-11-01 23:50:57 +0300

    add

--- src/TeachMeSkills/EnglishBook/src/main/java/com/example/dto/LoginDto.java
+++ src/TeachMeSkills/EnglishBook/src/main/java/com/example/dto/LoginDto.java
@@ -1,7 +1,13 @@
 package com.example.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.validation.constraints.Pattern;
 
+
+@Getter
+@Setter
 public class LoginDto {
 
     @Pattern(regexp = "(^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$)", message = "The Email isn't correct, try again")
---
+++ src/TeachMeSkills/EnglishBook/src/main/java/com/example/service/UserService.java
@@ -0,0 +1,37 @@
+package com.example.service;
+
+import com.example.dao.HibernateUserDao;
+import com.example.dto.LoginDto;
+import com.example.dto.RegUserDto;
+import com.example.entity.SessionUser;
+import com.example.entity.User;
+import com.example.mapper.RegUserMapper;
+import com.example.mapper.UserSessionMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Optional;
+
+public class UserService {
+
+    @Autowired
+    private HibernateUserDao hibernateUserDao;
+    public void save(RegUserDto regUserDto){
+        User user = RegUserMapper.regUserDtoToUser(regUserDto);
+        hibernateUserDao.save(user);
+    }
+
+    public Optional<SessionUser> login (LoginDto loginDto){
+        Optional<User> user = hibernateUserDao.findByEmail(loginDto.getEmail());
+
+        if(user.isPresent()){
+
+            User currentUser =user.get();
+
+            if(currentUser.getPassword().equals(loginDto.getPassword())){
+                return Optional.of(UserSessionMapper.userToSessionUser(currentUser));
+            }
+        }
+        return Optional.empty();
+
+    }
+}
