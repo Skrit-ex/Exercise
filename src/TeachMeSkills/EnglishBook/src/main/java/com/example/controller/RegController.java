7d9922a129f089a5afe44d6e293f7500d3dad625
Skrit-ex <skritex0@gmail.com>
2024-11-02 22:46:44 +0300

    add

---
+++ src/TeachMeSkills/EnglishBook/src/main/java/com/example/controller/RegController.java
@@ -0,0 +1,73 @@
+package com.example.controller;
+
+
+import com.example.dto.LoginDto;
+import com.example.dto.RegUserDto;
+import com.example.entity.SessionUser;
+import com.example.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpSession;
+import javax.validation.ConstraintViolationException;
+import javax.validation.Valid;
+import java.util.Optional;
+
+@Controller
+@RequestMapping("/user")
+public class RegController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/reg")
+    public String reg (Model model){
+        model.addAttribute("newUser", new RegUserDto());
+        return "reg";
+    }
+
+    @PostMapping("/reg")
+    public String reg(Model model, BindingResult bindingResult,
+                      @ModelAttribute("newUser") @Valid RegUserDto regUserDto){
+        if(bindingResult.hasErrors()){
+            return "reg";}
+        try{
+            userService.save(regUserDto);
+        }
+        catch (ConstraintViolationException e){
+            model.addAttribute("regError", "User with this username is already exist");
+            return "reg";
+        }
+        return "redirect:/user/login";
+    }
+
+    @GetMapping("/login")
+    public String login(Model model){
+        model.addAttribute("newLogin", new  LoginDto());
+        return "login";
+    }
+
+    @PostMapping("/login")
+    public String login(@ModelAttribute("newLogin") @Valid LoginDto loginDto,
+                        BindingResult bindingResult, Model model,
+                        HttpSession httpSession){
+        if(bindingResult.hasErrors()) {
+            return "login";
+        }
+                Optional<SessionUser> sessionUser = userService.login(loginDto);
+                if(sessionUser.isPresent()){
+                httpSession.setAttribute("userSession", sessionUser.get());
+                return "/";
+                }
+                else {
+                    model.addAttribute("loginError", "Login  or password is wrong, try again");
+                    return "login";
+                }
+    }
+}
---
+++ src/TeachMeSkills/EnglishBook/src/main/java/com/example/englishbook/EnglishBookApplication.java
@@ -0,0 +1,13 @@
+package com.example.englishbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnglishBookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnglishBookApplication.class, args);
+    }
+
+}
