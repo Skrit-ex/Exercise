4c8d307ca2d74b1906452089313b02beaffd0674
Skrit-ex <skritex0@gmail.com>
2024-12-23 23:52:04 +0300

    add

--- src/TeachMeSkills/EnglishBook/src/main/java/com/example/controller/RegController.java
+++ src/TeachMeSkills/EnglishBook/src/main/java/com/example/controller/RegController.java
@@ -5,6 +5,7 @@ import com.example.dto.RegUserDto;
 import com.example.entity.SessionUser;
 import com.example.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
@@ -14,7 +15,7 @@ import javax.validation.ConstraintViolationException;
 import javax.validation.Valid;
 import java.util.Optional;
 
-@RestController
+@Controller
 @RequestMapping("/user")
 public class RegController {
 
