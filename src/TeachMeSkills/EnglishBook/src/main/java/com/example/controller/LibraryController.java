424f24925635c3a1a2c4c5b0222c2d8c8beaeed5
Skrit-ex <skritex0@gmail.com>
2025-01-21 22:59:28 +0300

    added

--- src/TeachMeSkills/EnglishBook/src/main/java/com/example/controller/LibraryController.java
+++ src/TeachMeSkills/EnglishBook/src/main/java/com/example/controller/LibraryController.java
@@ -1,10 +1,15 @@
 package com.example.controller;
 
+import com.example.dto.BookDto;
+import com.example.entity.Book;
 import com.example.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -16,7 +21,17 @@ public class LibraryController {
 
     @GetMapping()
     public String library(Model model){
-//        bookService.addListOfBooks();
+        model.addAttribute("book", new BookDto());
+        return "library";
+    }
+
+    @PostMapping()
+    public String library(@ModelAttribute("book")BookDto bookDto,
+                          BindingResult bindingResult,
+                          Model model){
+        if(bindingResult.hasErrors()){
+            return "home";
+        }
         return "library";
     }
 }
